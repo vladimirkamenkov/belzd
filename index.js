39fdb780054f5f1fa8f5b32f5cb7b2d0bbd5658f
Vladimir Kamenkov <vl.cm@ya.ru>
2022-03-05 12:29:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const sendedUrls = [];
 const transporter = nodemailer.createTransport({
     host: 'smtp.yandex.ru',
     port: 465,
-    secure: true, 
+    secure: true,
     auth: {
-      user: 'vl.cm@ya.ru',
-      pass
+        user: 'vl.cm@ya.ru',
+        pass
     }
 });
 
 const mailOptions = {
     from: 'vl.cm@ya.ru',
     to: 'vl.cm@ya.ru',
-    subject: 'ATTANTION BEL ZD!!!',
+    subject: 'ATTANTION BELAVIA TICKETS!!!',
     text: 'That was easy!'
 };
 
@@ -31,28 +31,30 @@ const getHTMLUrl = async (url) => {
 };
 
 const urls = [
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-05-26',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-05-27',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-05-30',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-05-31',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-06-01',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-06-02',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-06-03',
-    'https://pass.rw.by/ru/route/?from=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&from_esr=&from_exp=&to=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&to_esr=&to_exp=&date=2021-06-04'
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220319&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220320&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220321&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220322&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220323&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220324&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220325&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220326&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220327&adults=1&children=0&infants=0&lang=ru',
+    'https://ibe.belavia.by/select?journeyType=Ow&journey=MSQBUS20220328&adults=1&children=0&infants=0&lang=ru'
 ];
 
-cron.schedule('* * * * *', function() {
+cron.schedule('* * * * *', function () {
     urls.forEach(async url => {
         const html = await getHTMLUrl(url);
         const $ = cheerio.load(html);
-        const elems = $('.sch-table__row[data-train-number="722Б"] .cell-4.empty, .sch-table__row[data-train-number="718Б"] .cell-4.empty');
-    
-        if (elems.length < 2 && !sendedUrls.includes(url)) {
+        const elems = $('.offer-item');
+
+        if (elems.length && !sendedUrls.includes(url)) {
             mailOptions.text = `URL: ${url}`;
-    
+
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
-                  console.log(error);
+                    console.log(error);
                 } else {
                     sendedUrls.push(url);
                     console.log('Email sent: ' + info.response);
@@ -60,6 +62,6 @@ cron.schedule('* * * * *', function() {
             });
         }
     });
-    
+
     console.log('all urls cheked');
 });
